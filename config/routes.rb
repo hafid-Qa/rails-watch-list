7fd696d86e7bd32540d8ab32092164cdfd710f8e
Hafid Qarchi <haqarchi@gmail.com>
2022-11-10 16:10:59 +0900

    create new list

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :movies
-  resources :lists, only: [:show]
+  resources :lists, only: %i[show new create]
   # Defines the root path route ("/")
   root 'movies#index'
 end
